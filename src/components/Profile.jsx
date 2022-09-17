cb08715e3a7790f4e77a45b25923b954b375e4b0
Saradhi <saradhi0001069@gmail.com>
2022-09-17 17:37:41 +0530

    t9

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./styles.css";
 import { useState,useEffect } from "react";
 import axios from "axios";
@@ -14,17 +14,36 @@ const Profile = () => {
 
       const [formData, Setformdata] = useState({});
       const [open, setOpen] = React.useState(false);
+      const [open1, setOpen1] = React.useState(false);
+      const [data, setData] = useState({});
+      const inputDoc = useRef();
 
       const handleClose = () => {
         setOpen(false);
       };
 
-      const handleSubmit = ()=>{
+      const handleChange = (e)=>{
+        let name = e.target.name;
+        setData({
+        ...data,
+        [name]: e.target.value,
+        [name]: e.target.value,
+       });
+       } 
 
-      }
-      const handleChange = ()=>{
+       const handleSubmit = (e)=>{
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("name",data.name);
+        formData.append("nft",inputDoc.current.files[0]);
+        // axios.post(`https://assignment91mobiles.herokuapp.com/user/${userid}/docs`,formData,{
+        //      headers:{"Content-type":"multipart/formdata"},
+        // })
+        console.log(formData.name);
+        // setOpen1(true);
+        };
 
-      } 
+
   return (
     <>
     <div className='profile'>
@@ -43,7 +62,7 @@ const Profile = () => {
         <label htmlFor="fname">NAME</label>
         <input type="text" name="name" placeholder="Name of your nft.." onChange={handleChange} required></input>
         <label htmlFor="lname">PASSWORD</label><br></br>
-        <input type="file" name="nft" onChange={handleChange} required></input>
+        <input type="file" ref={inputDoc} name="nft" accept=".jpg" onChange={handleChange} required></input>
         <input type="submit" value="UPLOAD"></input>
         </form>
         </DialogTitle>
